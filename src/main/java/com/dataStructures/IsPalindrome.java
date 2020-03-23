3c1732673e8edf802dec14553bbff7fd564842a0
高国兴 <nanyiniu76@gmail.com>
2020-03-23 13:44:28 +0800

    9. 回文数

--- src/main/java/com/dataStructures/IsPalindrome.java
+++ src/main/java/com/dataStructures/IsPalindrome.java
@@ -1,9 +1,40 @@
 package com.dataStructures;
 
 /**
- * 判断是否为回文数
+ * 9. 回文数
+ *
+ * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ *
+ * 示例 1:
+ *
+ * 输入: 121
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入: -121
+ * 输出: false
+ * 解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+ * 示例 3:
+ *
+ * 输入: 10
+ * 输出: false
+ * 解释: 从右向左读, 为 01 。因此它不是一个回文数。
+ * 进阶:
+ *
+ * 你能不将整数转为字符串来解决这个问题吗？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/palindrome-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
  */
 public class IsPalindrome {
+
+    /**
+     * 暴力拼接 比较 原来的拼接后的
+     * @param x
+     * @return
+     */
     public boolean solution(int x){
         if(x < 0){
             return false;
@@ -16,6 +47,11 @@ public class IsPalindrome {
         return stringBuilder.toString().equals(org);
     }
 
+    /**
+     * 前后互相对比
+     * @param x
+     * @return
+     */
     public boolean solution2(int x){
         if(x < 0){
             return false;
